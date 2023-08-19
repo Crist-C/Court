c58254cbca8fbbdc99e5615984133ac7c1aec509
Cristian Castro <castro.cristiank@gmail.com>
2023-08-18 22:50:34 -0500

    Initial commit. Setting dependencies: Hilt, Retrofit, Coil, Pager, Utils, Runtime, Room, Gson. Architecture design.

---
+++ app/src/main/java/com/ccastro/court/court.kt
@@ -0,0 +1,8 @@
+package com.ccastro.court
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class court: Application() {
+}
---
+++ app/src/main/java/com/ccastro/court/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.ccastro.court.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
