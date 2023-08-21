3d6681765714f44c7557ec9d6f36026e6c457c29
Cristian Castro <castro.cristiank@gmail.com>
2023-08-21 11:27:04 -0500

    Empty stylish presentation layer create.

---
+++ app/src/main/java/com/ccastro/court/presentation/screens/hairStylist/HairStylistState.kt
@@ -0,0 +1,5 @@
+package com.ccastro.court.presentation.screens.hairStylist
+
+data class HairStylistState(
+    val test: Int = 0
+)
---
+++ app/src/main/java/com/ccastro/court/presentation/screens/stylistShop/StylistShopScreen.kt
@@ -0,0 +1,9 @@
+package com.ccastro.court.presentation.screens.stylistShop
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StylistShopScreen( modifier: Modifier = Modifier) {
+
+}
